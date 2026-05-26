944c024bbca542ef6c1340f5fd0992a9fc4c18de
R-0WL <abe24290@uvg.edu.gt>
2026-05-26 01:17:43 -0600

    cambiode credenciales para pory 3

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const authRouter = require('./routes/auth');
 const resenasRouter = require('./routes/resenas');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.APP_PORT || process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
